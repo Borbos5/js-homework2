d9c76c63180bb7da9e3aa3f55a20ac19f855bf1a
Никита Баранов <124291854+Borbos5@users.noreply.github.com>
2023-02-14 19:47:47 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,29 @@
+let number1 = prompt();
+let number2 = prompt();
+
+let n1 = Number(number1);
+let n2 = Number(number2);
+
+let number = (n1 + n2);
+
+// alert(n1 + n2);
+n1 + n2
+
+if (number > 10) {
+    alert("Ok");
+}
+
+else {
+    alert("Error");
+}
+
+
+let name = prompt();
+let Andrew = 5;
+
+if (Andrew < 10) {
+    alert("Hi!")
+}
+else {
+    alert("Who are u")
+}
